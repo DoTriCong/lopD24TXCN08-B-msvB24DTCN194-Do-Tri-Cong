5e93a1ae5fb2e94b1cdb7ecb23e96555260255dd
DoTriCong <tricong5526@gmail.com>
2024-12-12 21:58:05 +0700

    Add files via upload

---
+++ FPLDLF24msvB24DTCN194-03.cpp
@@ -0,0 +1,20 @@
+//B24DTCN194-Do Tri Cong-Bai 3
+//Su khac biet giua (1) va (3):
+//(1) int x;: Day chi la khai bao bien x nhung khong khoi tao gia tri cho no. Bien x la mot bien chua xac dinh gia tri, do do gia tri cua no co the la bat ky gia tri ngau nhien nao (gia tri rac) tai thoi diem su dung.
+//(3) int y = 2;: Day la khai bao va khoi tao bien y voi gia tri 2. Bien y se co gia tri xac dinh la 2.
+//Ket qua cua (2) 
+//Dong nay in gia tri cua bien x. Tuy nhien, vi x chua duoc khoi tao, gia tri cua x la gia tri rac, nen ket qua in ra la khong xac dinh, co the la bat ky so nao.
+//Ket qua cua (4) 
+//Dong nay in gia tri cua y + 4. Vi y = 2 (theo dong (3)), nen y + 4 = 2 + 4 = 6. Kien thuc in ra la 6.
+#include <stdio.h>
+
+int main() {
+    int x;
+    printf("%d\n", x);
+
+    int y = 2;
+    printf("%d\n", y + 4);
+
+    return 0;
+}
+
