e5e56c1597a3c5e4d891d85edefbe505401b6f6f
DoTriCong <tricong5526@gmail.com>
2024-12-12 21:45:45 +0700

    Add files via upload

---
+++ FPLDLF24msvB24DTCN194-02.cpp
@@ -0,0 +1,12 @@
+//B24DTCN194-Do Tri Cong-Bai 2 
+#include <stdio.h>
+
+int main() {
+    unsigned char ma_the_sinh_vien;
+    printf("Nhap ma the sinh vien (0-255): ");
+    scanf("%hhu", &ma_the_sinh_vien);
+
+    printf("Ma the sinh vien: %d\n", ma_the_sinh_vien);
+    return 0;
+}
+
